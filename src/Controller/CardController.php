e911a8dfffec51f7fcd8f4469c2cd3b36b21df26
Johan Guttormsson <655558+johangu@users.noreply.github.com>
2025-04-22 15:44:05 +0200

    feat: add routes and controller

---
+++ src/Controller/CardController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Controller;
+
+use App\Card\CardHand;
+use App\DeckOfCards\StandardDeck;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CardController extends AbstractController
+{
+    #[Route('/card', name: 'card')]
+    public function card(): Response
+    {
+        return $this->render('card/index.html.twig');
+    }
+
+    #[Route('/card/deck', name: 'deck')]
+    public function deck(SessionInterface $session): Response
+    {
+        if (! $session->has('deck')) {
+            $session->set('deck', new StandardDeck());
+        }
+
+        $deck = $session->get('deck');
+
+        return $this->render('card/deck.html.twig', [
+            'deck' => $deck->getSorted(),
+            'sorted' => true,
+        ]);
+    }
+
+    #[Route('/card/deck/new', name: 'deck_new')]
+    public function newDeck(SessionInterface $session): RedirectResponse
+    {
+        $deck = new StandardDeck();
+
+        $session->set('deck', $deck);
+
+        $this->addFlash('success', 'A new deck has been created');
+
+        return $this->redirectToRoute('deck');
+    }
+
+    #[Route('/card/deck/shuffle', name: 'deck_shuffle')]
+    public function shuffleDeck(SessionInterface $session): Response
+    {
+        $deck = $session->get('deck');
+        $deck->shuffle();
+        $session->set('deck', $deck);
+
+        $this->addFlash('success', 'The deck has been shuffled');
+
+        return $this->render('card/deck.html.twig', [
+            'deck' => $deck->getCards(),
+            'sorted' => false,
+        ]);
+    }
+
+    #[Route('/card/deck/draw', name: 'deck_draw')]
+    public function drawFromDeck(SessionInterface $session): Response
+    {
+        $deck = $session->get('deck');
+        $hand = new CardHand($deck, 1);
+        $session->set('deck', $deck);
+
+        return $this->render('card/hand.html.twig', [
+            'hand' => $hand,
+        ]);
+    }
+
+    #[Route('/card/deck/draw/{number}', name: 'draw_many')]
+    public function drawManyFromDeck(int $number, SessionInterface $session): Response
+    {
+        $deck = $session->get('deck');
+        $hand = new CardHand($deck, $number);
+        $session->set('deck', $deck);
+        $this->addFlash('success', 'You have drawn '.count($hand->getCards()).' cards');
+
+        return $this->render('card/hand.html.twig', [
+            'hand' => $hand,
+        ]);
+    }
+}
