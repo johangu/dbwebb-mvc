a5675cb21f1cde47606d90ced7dc92ff84d46217
Johan Guttormsson <655558+johangu@users.noreply.github.com>
2025-04-24 10:20:11 +0200

    feat: add missing deck API logic

--- src/Controller/DeckApiController.php
+++ src/Controller/DeckApiController.php
@@ -14,12 +14,80 @@ class DeckApiController extends AbstractController
     public function apiDeck(SessionInterface $session): JsonResponse
     {
         if (! $session->has('deck')) {
-            $session->set('deck', new StandardDeck);
+            $session->set('deck', new StandardDeck());
         }
 
         $deck = $session->get('deck');
 
         $response = new JsonResponse($deck);
+        $response->setEncodingOptions(
+            $response->getEncodingOptions() | JSON_PRETTY_PRINT
+        );
+
+        return $response;
+    }
+
+    #[Route('/api/deck/shuffle', name: 'api_shuffle_deck', methods: ['POST'])]
+    public function apiShuffleDeck(SessionInterface $session): JsonResponse
+    {
+        if (! $session->has('deck')) {
+            return new JsonResponse(status: 404);
+        }
+
+        $deck = $session->get('deck');
+        $deck->shuffle();
+        $session->set('deck', $deck);
+
+        $response = new JsonResponse($deck);
+        $response->setEncodingOptions(
+            $response->getEncodingOptions() | JSON_PRETTY_PRINT
+        );
+
+        return $response;
+    }
+
+    #[Route('/api/deck/draw/{number?1}', name: 'api_deck_draw', methods: ['POST'])]
+    public function apiDrawCardFromDeck(SessionInterface $session, int $number = 1): JsonResponse
+    {
+        if (! $session->has('deck')) {
+            return new JsonResponse(status: 404);
+        }
+
+        $deck = $session->get('deck');
+        $cards = $deck->drawHand($number);
+        $count = $deck->getCount();
+        $session->set('deck', $deck);
+
+        $response = new JsonResponse([
+            'cards' => $cards,
+            'cardsLeft' => $count,
+        ]);
+        $response->setEncodingOptions(
+            $response->getEncodingOptions() | JSON_PRETTY_PRINT
+        );
+
+        return $response;
+    }
+
+    #[Route('/api/deck/deal/{players?1}/{cards?1}', name: 'api_deck_deal', methods: ['POST'])]
+    public function apiDealCards(SessionInterface $session, int $players = 1, int $cards = 1): JsonResponse
+    {
+        if (! $session->has('deck')) {
+            return new JsonResponse(status: 404);
+        }
+
+        $deck = $session->get('deck');
+        $hands = [];
+        for ($i = 0; $i < $players; $i++) {
+            $hands[$i] = $deck->drawHand($cards);
+        }
+        $count = $deck->getCount();
+        $session->set('deck', $deck);
+
+        $response = new JsonResponse([
+            'hands' => $hands,
+            'cardsLeft' => $count,
+        ]);
         $response->setEncodingOptions(
             $response->getEncodingOptions() | JSON_PRETTY_PRINT
         );
